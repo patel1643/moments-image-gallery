f475b3cf0fde69cb8745088aed682634ca148051
Parth Patel <42159154+patel1643@users.noreply.github.com>
2023-12-25 20:49:11 -0500

    added login component and page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    LoginFormComponent,
+    SignUpPageComponent,
+    SignUpFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginFormData } from '../../interfaces/AuthTypes';
+import { AuthService } from '../../services/auth.service';
+
+
+
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss'
+})
+export class LoginFormComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private authService: AuthService){}
+
+  loginFormSubmit(): void{
+    const loginFormData: LoginFormData = this.loginForm.value;
+
+    this.authService.login(loginFormData).subscribe(resp => {
+      console.log(resp);
+    })
+  }
+
+
+
+
+  // logger(value:any){
+  //   console.log(value)
+  // }
+  ngOnInit(): void {
+      this.loginForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [ Validators.required, Validators.minLength(6)]),
+        rememberMeCheckbox: new FormControl('')
+      });
+  }
+}
