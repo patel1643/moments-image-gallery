077a8c9bb7d67e04ca0df967ac22cef90bc60bff
Parth Patel <42159154+patel1643@users.noreply.github.com>
2023-12-25 20:50:06 -0500

    added routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomePageComponent },
+  { path:'login', component: LoginPageComponent },
+  { path:'signup', component: SignUpPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
