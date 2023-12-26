d11eb41630e630a5bcd1134ccd1224430bfb9819
Parth Patel <42159154+patel1643@users.noreply.github.com>
2023-12-25 20:50:19 -0500

    added types

---
+++ client/src/app/interfaces/AuthTypes.ts
@@ -0,0 +1,13 @@
+
+export interface LoginFormData  {
+  email: string,
+  password: string
+  checkBox?: boolean
+}
+
+export interface SignUpFormData {
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string
+}
