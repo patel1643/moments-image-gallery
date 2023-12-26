3863d4139f45f831f32dfe4a5ba645c698576490
Michael Pereira <mwpereira07@gmail.com>
2023-12-26 10:48:56 -0500

    setting website's title, refactoring apiUrl in authservice

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 import { LoginFormData, SignUpFormData } from '../interfaces/AuthTypes';
 
 @Injectable({
@@ -10,16 +9,16 @@ import { LoginFormData, SignUpFormData } from '../interfaces/AuthTypes';
 })
 export class AuthService {
 
-  private readonly apiURL: string = '';
+  private readonly apiUrl: string = '';
 
   constructor(private http: HttpClient) { }
 
 
   login(userLoginData: LoginFormData) : Observable<any>{
-    return this.http.post<any>(this.apiURL, userLoginData);
+    return this.http.post<any>(this.apiUrl, userLoginData);
   }
 
   signUp(userSignUpData: SignUpFormData): Observable<any> {
-    return this.http.post<any>(this.apiURL, userSignUpData);
+    return this.http.post<any>(this.apiUrl, userSignUpData);
   }
 }
