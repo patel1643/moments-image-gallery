a5c78c1d93e83bee5bdd29b927e11ddd7af02b36
Parth Patel <42159154+patel1643@users.noreply.github.com>
2024-01-07 22:53:28 -0500

    added scrolling images

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { dummyImages } from '../../tempdata';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
+  images: string[];
+  ngOnInit(): void {
+      this.images = dummyImages;
+  }
 }
---
+++ client/src/app/tempdata.ts
@@ -0,0 +1,8 @@
+export const dummyImages: string[] = [
+    "https://images.pexels.com/photos/3769120/pexels-photo-3769120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/8038906/pexels-photo-8038906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/3920974/pexels-photo-3920974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/4497596/pexels-photo-4497596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/3944093/pexels-photo-3944093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/8533826/pexels-photo-8533826.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+]
