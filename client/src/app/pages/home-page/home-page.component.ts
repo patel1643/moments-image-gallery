718c54a69f939dc9fd81d732ecc1ec9c9ae20d65
Parth Patel <42159154+patel1643@users.noreply.github.com>
2024-01-17 01:45:32 -0500

    fixed the photos

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { dummyImages } from '../../tempdata';
+// import { image } from '../../assets/logos/moments-image-gallery-high-resolution-logo-transparent.png'
 
 @Component({
   selector: 'app-home-page',
